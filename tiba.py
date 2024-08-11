f0e18b642defad979a468649356fa2d97949deb5
unknown <tibaredha@yahoo.fr>
2024-08-11 17:29:40 +0200

    add py

---
+++ tiba.py
@@ -0,0 +1,12 @@
+print("tibaredha")
+
+import  numpy as np
+import  pandas as pd
+import matplotlib.pyplot as plt
+
+df1 = pd.read_csv("C:/Users/SSD STORE/OneDrive/Bureau/yahiatiba/inst/extdata/deces.csv")
+df2 = pd.read_csv("./inst/extdata/deces.csv")
+print(df1)
+
+df.plot(kind = 'scatter', x = 'COMMUNER', y = 'SEX')
+plt.show()
