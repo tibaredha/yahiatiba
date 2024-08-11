d2161e8fa8ae4e8d95e96e4f7167d3bd5b081477
unknown <tibaredha@yahoo.fr>
2024-08-11 19:01:26 +0200

    maj

--- tiba.py
+++ tiba.py
@@ -7,8 +7,18 @@ import matplotlib.pyplot as plt
 df1 = pd.read_csv("C:/Users/SSD STORE/OneDrive/Bureau/yahiatiba/inst/extdata/deces.csv")
 df2 = pd.read_csv("./inst/extdata/deces.csv")
 print(df1)
+df1.shape
+df1.isnull().sum()
+df1.head(15)
+df1.tail(15)
+df1.info()
+df1.describe()
 
-df.plot(kind = 'scatter', x = 'COMMUNER', y = 'SEX')
+df1.Years.hist(bins = 50, figsize=(12,8))
+plt.show()
+
+import matplotlib.pyplot as plt
+df1.Years.hist(bins = 50, figsize=(12,8))
 plt.show()
 
 def myfunc():
